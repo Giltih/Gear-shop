b3c72d351617c6a9920a5d28b9ccdbc26ed6397f
galbh <galbhmusic@gmail.com>
2022-11-26 19:14:51 +0200

    galbh/init

---
+++ mock/products.js
@@ -0,0 +1,7 @@
+var products = [
+  { name: 'boxing gloves', price: 200, id: '001', description: 'some description', image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/640px-Image_created_with_a_mobile_phone.png' },
+  { name: 'rush guard', price: 150, id: '002', description: 'some description 2', image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/640px-Image_created_with_a_mobile_phone.png' },
+  { name: 'head guard', price: 299, id: '003', description: 'some description 3', image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/640px-Image_created_with_a_mobile_phone.png' },
+  { name: 'gi', price: 399, id: '004', description: 'some description 4', image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/640px-Image_created_with_a_mobile_phone.png' },
+  { name: 'mouth piece', price: 120, id: '005', description: 'some description 5', image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/640px-Image_created_with_a_mobile_phone.png' },
+];
---
+++ script.js
@@ -0,0 +1,41 @@
+renderProducts(products);
+
+function renderProducts(allProducts) {
+  // Get container to append products to
+  const container = document.querySelector('.products');
+
+  // Iterate all products
+  for (let i = 0; i < allProducts.length; i++) {
+    // Each product data
+    const productData = allProducts[i];
+
+    // Create an element for each product
+    const newProduct = document.createElement('div');
+    newProduct.className = 'product';
+
+    // Create element for product name
+    const name = createHtmlElement('div', 'name', 'innerHTML', productData.name);
+
+    // Create element for product description
+    const description = createHtmlElement('div', 'description', 'innerHTML', productData.description);
+
+    // Create element for product image
+    const image = createHtmlElement('img', 'image', 'src', productData.image);
+
+    // Append all data elements to the element in line 26
+    newProduct.appendChild(name);
+    newProduct.appendChild(description);
+    newProduct.appendChild(image);
+
+    // Append element in line 26 to main container (line 18)
+    container.appendChild(newProduct);
+  }
+}
+
+// Generic method to create HTML element
+function createHtmlElement(tagName, className, attributeName, value) {
+  const element = document.createElement(tagName);
+  element.className = className;
+  element[attributeName] = value;
+  return element;
+}
