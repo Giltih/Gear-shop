fa0a1ed72a350e9508207cff8034fba56dd9677e
gilad abro <giladbass@gmail.com>
2022-11-27 00:08:16 +0200

    changed url plus added price function

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ renderProducts(products);
 
 function renderProducts(allProducts) {
   // Get container to append products to
-  const container = document.querySelector('.products');
+  const container = document.querySelector(".products");
 
   // Iterate all products
   for (let i = 0; i < allProducts.length; i++) {
@@ -10,23 +10,40 @@ function renderProducts(allProducts) {
     const productData = allProducts[i];
 
     // Create an element for each product
-    const newProduct = document.createElement('div');
-    newProduct.className = 'product';
+    const newProduct = document.createElement("div");
+    newProduct.className = "product";
 
     // Create element for product name
-    const name = createHtmlElement('div', 'name', 'innerHTML', productData.name);
+    const name = createHtmlElement(
+      "div",
+      "name",
+      "innerHTML",
+      productData.name
+    );
 
     // Create element for product description
-    const description = createHtmlElement('div', 'description', 'innerHTML', productData.description);
+    const description = createHtmlElement(
+      "div",
+      "description",
+      "innerHTML",
+      productData.description
+    );
 
     // Create element for product image
-    const image = createHtmlElement('img', 'image', 'src', productData.image);
+    const image = createHtmlElement("img", "image", "src", productData.image);
+    //create element for price
+    const price = createHtmlElement(
+      "div",
+      "price",
+      "innerHTML",
+      productData.price
+    );
 
     // Append all data elements to the element in line 26
     newProduct.appendChild(name);
     newProduct.appendChild(description);
     newProduct.appendChild(image);
-
+    newProduct.appendChild(price);
     // Append element in line 26 to main container (line 18)
     container.appendChild(newProduct);
   }
