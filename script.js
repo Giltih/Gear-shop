a5698d8161cbb44a9563a7f67d8513ac1ccbe083
gilad abro <giladbass@gmail.com>
2022-11-28 15:10:34 +0200

    going around in cirlces

--- script.js
+++ script.js
@@ -32,23 +32,33 @@ function renderProducts(allProducts) {
     // Create element for product image
     const image = createHtmlElement("img", "image", "src", productData.image);
     //create element for price
-    const price = createHtmlElement(
+    const btn = createHtmlElement(
       "button",
-      "price",
+      "btn",
       "innerHTML",
       productData.price
     );
-
     // Append all data elements to the element in line 26
     newProduct.appendChild(name);
     newProduct.appendChild(description);
     newProduct.appendChild(image);
-    newProduct.appendChild(price);
+    newProduct.appendChild(btn);
     // Append element in line 26 to main container (line 18)
     container.appendChild(newProduct);
   }
 }
+const btnAdd = document.querySelectorAll(".btn");
+btnAdd.addEventListener("click", function () {
+  document.querySelector("cart");
+  cart.appendChild(btnAdd);
+});
 
+function myCart() {
+  const cart = (document.createElement("div").className = "cart");
+  document.querySelector("body");
+  body.appendChild(cart);
+}
+myCart();
 // Generic method to create HTML element
 function createHtmlElement(tagName, className, attributeName, value) {
   const element = document.createElement(tagName);
@@ -57,14 +67,14 @@ function createHtmlElement(tagName, className, attributeName, value) {
   return element;
 }
 
+// let sum = 0;
+// document.querySelector(".innerHTML").addEventListener("click", function () {
+//   sum = sum + price;
+//   console.log(sum);
+// });
 //create a cart with my products i add
 //create a button onclick for each div
 //add to the cart the prices
 //give option to cancel
 //add the sum numbers for check out
 //add the bjj white belt dis9count
-let sum = 0;
-document.querySelector(".innerHTML").addEventListener("click", function () {
-  sum = sum + price;
-  console.log(sum);
-});
